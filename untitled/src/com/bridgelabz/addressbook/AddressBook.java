c2ef40382c208d829d5f91db0b8991d01bf13f92
urmila4462 <dasari.urmila4462@gmail.com>
2022-05-24 20:28:11 +0530

    Added details of a person in address book

--- untitled/src/com/bridgelabz/addressbook/AddressBook.java
+++ untitled/src/com/bridgelabz/addressbook/AddressBook.java
@@ -1,7 +1,49 @@
 package com.bridgelabz.addressbook;
+import java.util.Scanner;
 
 public class AddressBook {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+        getInput();
+
     }
+    public static void getInput() {
+        DetailsAddressBook details = new DetailsAddressBook();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the following address details ");
+        System.out.println("Enter the First Name :");
+        details.setFirstName(input.nextLine());
+        System.out.println("First Name :" + details.getFirstName());
+
+        System.out.println("Enter the last name:");
+        details.setLastName(input.nextLine());
+        System.out.println("Last Name: " + details.getLastName());
+
+        System.out.println("Enter your email address:");
+        details.setEmailAddress(input.nextLine());
+        System.out.println("email address: " + details.getEmailAddress());
+
+        System.out.println("Enter your address:");
+        details.setAddress(input.nextLine());
+        System.out.println("address: " + details.getAddress());
+
+        System.out.println("Enter your city name:");
+        details.setCity(input.nextLine());
+        System.out.println("City :" + details.getCity());
+
+        System.out.println("Enter your state:");
+        details.setState(input.nextLine());
+        System.out.println("State:" + details.getState());
+
+        System.out.println("Enter your pin code:");
+        details.setZip(input.nextLong());
+        System.out.println("Zip:" + details.getZip());
+
+        System.out.println("Enter your phone number:");
+        details.setPhoneNumber(input.nextLong());
+        System.out.println("Phone Number:" + details.getPhoneNumber());
+
+    }
+
 }
+
---
+++ untitled/src/com/bridgelabz/addressbook/DetailsAddressBook.java
@@ -0,0 +1,78 @@
+package com.bridgelabz.addressbook;
+
+public class DetailsAddressBook {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private long phoneNumber;
+    private long zip;
+    private String emailAddress;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public long getZip() {
+        return zip;
+    }
+
+    public void setZip(long zip) {
+        this.zip = zip;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+}
+
